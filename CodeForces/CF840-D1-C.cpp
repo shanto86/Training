45acf4877785cc5429bca7132558137c0019c8b4
shanto86 <shanto86@gmail.com>
2018-11-21 20:17:35 +0100

    Update CF840-D1-C.cpp

--- CodeForces/CF840-D1-C.cpp
+++ CodeForces/CF840-D1-C.cpp
@@ -1,3 +1,4 @@
+// Solution same as analysis: http://codeforces.com/blog/entry/53943?locale=en
 //#pragma GCC optimize("Ofast")
 //#pragma GCC optimize ("unroll-loops")
 //#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
